cbdd74ecfe7d978c5fb91514ba85df7e900b6e7a
Wolfram Zimmermann <wolfram.zimmermann@gmx.net>
2022-11-14 16:33:10 +0100

    Add Set-Permission functionionality and Tests
    I added some parameters to Set-Permission with tests according to the new
    params.
    I decided not to show the enhanced object parameters.
    
    I also changed the build script to avoid coverage tests.

--- src/PSFSPM/Public/Set-Permission.ps1
+++ src/PSFSPM/Public/Set-Permission.ps1
@@ -6,6 +6,12 @@
    You can set permissions for several users at once given as a list.
 .NOTES
    Information or caveats about the function e.g. 'This function is not supported in Linux'
+.PARAMETER Path
+   One or more paths, where you want to set the permission(s) on.
+   The validation, if the path exists may provide wrong results - make sure, that the given path
+   is not available in current dir.
+.PARAMETER Identity
+   One or more identities for which you want to set rights for
 .EXAMPLE
    Set-Permission -Path C:\Temp -Identity/Principal foo -Permission Write -Inheritance ThisFolderSubfoldersAndFiles
    Set permission for C:\Temp to 'Write' for user 'foo' with inheritance 'ThisFolderSubfoldersAndFiles'
@@ -37,17 +43,49 @@
 
    #>
 function Set-Permission {
-   [CmdletBinding()]
+   [CmdletBinding(SupportsShouldProcess)]
    param (
-
+      # Path(s) to set permission(s) on
+      [Parameter(Position = 0, Mandatory, ValueFromPipeline)]
+      [Parameter(ParameterSetName = 'Default')]
+      [Parameter(ParameterSetName = 'PermissionObject')]
+      [Parameter(ParameterSetName = 'PathPermissionObject')]
+      [ValidateScript({
+         if (Test-Path $_) { $true }
+         else { Throw "Path $_ is not valid" }
+      })]
+      [String[]]
+      $Path,
+      # The identity(ies) to set permission for
+      [Parameter(ParameterSetName = 'Default')]
+      [Alias('Principal')]
+      [String[]]$Identity,
+      # The permission(s) to set
+      [Parameter(ParameterSetName = 'Default')]
+      [String]$Permission,
+      # The inheritance(s) to set
+      [Parameter(ParameterSetName = 'Default')]
+      [String]$Inheritance,
+      # A permission object with all necessary permissions
+      [Parameter(ParameterSetName = 'PermissionObject',DontShow)]
+      [String]$PermissionObject,
+      [Parameter(ParameterSetName = 'PathPermissionObject',DontShow)]
+      [String]$PathPermissionObject
    )
 
    begin {
-
+      #parameter checks
+      if ($PSBoundParameters.ContainsKey('Identity') -or $PSBoundParameters.ContainsKey('Permission') -or $PSBoundParameters.ContainsKey('Inheritance')) {
+         If (($Identity.Count -ne $Permission.Count) -or ($Identity.Count -ne $Inheritance.Count)) {
+            Throw "Counts of identities, permissions and inheritances don't match - please check"
+         }
+      }
    }
 
    process {
+      if ($PSCmdlet.ShouldProcess('Target', 'Operation')) {
 
+      }
    }
 
    end {
--- src/Tests/Unit/Public/Set-Permission.Tests.ps1
+++ src/Tests/Unit/Public/Set-Permission.Tests.ps1
@@ -10,15 +10,52 @@ BeforeAll {
       Remove-Module -Name $ModuleName -Force
    }
    Import-Module $PathToManifest -Force
+   #init dummy vars
+   $PermObject = "foo"
    #-------------------------------------------------------------------------
 }
+
 Describe 'Set-Permission' -Tag Unit {
    Context 'Function call' -Tag Unit {
-      It "Checks if function is available after module import" {
-          {Set-Permission} | Should -Not -Throw "*wurde nicht als Name*"
-      }
-      It "Checks if help exists" {
-          {Get-Help Set-Permission} | Should -Not -Throw "*in dieser Sitzung von Get-Help nicht gefunden*"
+      It 'Checks if function is available after module import' {
+         { Set-Permission -Path $TestDrive -PermissionObject $PermObject } | Should -Not -Throw '*is not recognized as the name*'
+      }#end it
+      It 'Checks if help exists' {
+         { Get-Help Set-Permission } | Should -Not -Throw '*in a help file in this session*'
       }
    }#end context function call
+   Context 'Parameter check - path' -Tag Unit {
+      It 'Should prompt, if path is missing and error when pass is empty' {
+         { Set-Permission -Path "" -PermissionObject $PermObject } | Should -Throw "* Cannot bind argument to parameter 'Path'*"
+      }
+      It 'Should return error, when param is no filesystemobject' {
+         { Set-Permission -Path 'Throw' } | Should -Throw "Cannot validate argument on parameter 'Path'*"
+      }
+      It 'Should not fail if path is filesystemobject' {
+         New-Item -Path $TestDrive -Name 'foo.txt' -ItemType File
+         { Set-Permission -Path $TestDrive -PermissionObject $PermObject} | Should -Not -Throw
+         { Set-Permission -Path "$TestDrive\foo.txt" -PermissionObject $PermObject} | Should -Not -Throw
+      }
+      It 'Should not fail, if several valid paths are given' {
+         New-Item -Path $TestDrive -Name 'foo.txt' -ItemType File -Force
+         { Set-Permission -Path $TestDrive, $TestDrive -PermissionObject $PermObject} | Should -Not -Throw
+         { Set-Permission -Path $TestDrive, "$TestDrive\foo.txt" -PermissionObject $PermObject} | Should -Not -Throw
+      }
+      It 'Should fail, if several paths are given and at least one is invalid' {
+         { Set-Permission -Path $TestDrive, 'foo' -PermissionObject $PermObject} | Should -Throw '*Path * is not valid'
+      }
+      It 'Can take parameter from pipeline' {
+         {$TestDrive | Set-Permission -PermissionObject $PermObject} | Should -Not -Throw
+      }
+      It 'Can take several parameter(s) from pipeline' {
+         {$TestDrive,"$TestDrive\foo.txt" | Set-Permission -PermissionObject $PermObject} | Should -Not -Throw
+      }
+
+   }#end context param path
+   Context 'Parameter check for default parameter set' -Tag Unit {
+      It "Check if parameter counts correlate" {
+         {Set-Permission -Path $TestDrive -Identity $PermObject,"bar" -Permission "Modify" -Inheritance "ThisFolderSubfoldersAndFiles"} |
+         Should -Throw "Counts of identities*"
+      }
+   }#end Context
 }#end describe set-permission
