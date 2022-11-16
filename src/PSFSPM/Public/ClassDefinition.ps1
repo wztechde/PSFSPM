ffc0152b25d3c7a1faa68568dbe65a04bbc8fbf9
Wolfram Zimmermann <wolfram.zimmermann@faw.de>
2022-11-16 13:51:19 +0100

    Add New-FMPathPermission
    I added the function New-FMPathPermission wth different parameter sets
    so that it's possible to create all possible combinations: one or more
    paths with one or more permission combinations.
    The function takes FMPermission objects or explicit permissions.

--- src/PSFSPM/Public/ClassDefinition.ps1
+++ src/PSFSPM/Public/ClassDefinition.ps1
@@ -74,20 +74,47 @@ Class FMPathPermission {
    FMPathPermission(
       [String[]]$Path,
       [FMPermission[]]$Permission
-   ){
-      $this.Path=$Path
-      $this.Permission=$Permission
+   ) {
+      $this.Path = $Path
+      $this.Permission = $Permission
    }
 }
 
 #helper function to call constructor
 Function New-FMPathPermission {
+   [CmdletBinding()]
    Param (
-      [Parameter(ParameterSetName='Default')]
-      [Parameter(ParameterSetName='InputObject')]
+      [Parameter(ParameterSetName = 'Default')]
+      [Parameter(ParameterSetName = 'InputObject')]
       [String[]]$Path,
-      [Parameter(ParameterSetName='InputObject')]
-      [FMPermission[]]$InputObject
-      )
-         [FMPathPermission]::New($Path,$InputObject)
+      [Parameter(ParameterSetName = 'InputObject')]
+      [FMPermission[]]$InputObject,
+      [Parameter(ParameterSetName = 'Default')]
+      [String[]]$Identity,
+      [Parameter(ParameterSetName = 'Default')]
+      [System.Security.AccessControl.FileSystemRights[]]$Permission,
+      [Parameter(ParameterSetName = 'Default')]
+      [IMInheritance[]]$Inheritance
+   )
+   # parameter set 'InputObject'
+   if ($PSBoundParameters.ContainsKey('InputObject')) {
+      [FMPathPermission]::New($Path, $InputObject)
+   }
+   # parameter set 'Default'
+   if ($PSBoundParameters.ContainsKey('Identity')) {
+      If (($Identity.Count -ne $Permission.Count) -or ($Identity.Count -ne $Inheritance.Count)) {
+         Throw "Counts of identities, permissions and inheritances don't match - please check"
+      }
+      if ($Identity.Count -eq 1) {
+         $InputObject = New-FMPermission -Identity $Identity -Permission $Permission -Inheritance $Inheritance
+      }
+      else {
+         #create permission array
+         $InputObject=@()
+         for ($i = 0; $i -lt $Identity.count; $i++) {
+            $InputObject += New-FMPermission -Identity $Identity[$i] -Permission $Permission[$i] -Inheritance $Inheritance[$i]
+         }
+      }
+      [FMPathPermission]::New($Path, $InputObject)
+   }
 }
--- src/Tests/Unit/Public/Class.Tests.ps1
+++ src/Tests/Unit/Public/Class.Tests.ps1
@@ -40,12 +40,65 @@ Describe 'Class-Tests' -Tag Unit {
       }
    }#end Context
    Context 'Class FMPathPermission - helper function - paramset [InputObject]' -Tag Unit {
-      It "helper function returns correct object - paramset one FMPermission" {
-         $result = New-FMPathPermission -Path C:\Temp -InputObject (New-FMPermission -Id 'foo' -Perm 'Modify' -Inh 'ThisFolderOnly')
+      It "checks correct object being returned - one FMPermission" {
+         $Permission = New-FMPermission -Id 'foo' -Perm 'Modify' -Inh 'ThisFolderOnly'
+         $result = New-FMPathPermission -Path C:\Temp -InputObject $Permission
          $result.Path | Should -Be "C:\Temp"
+         $result.Permission.Count | Should -be 1
          $result.Permission.Identity | Should -Be "foo"
          $result.Permission.Permission | Should -Be "Modify"
          $result.Permission.Inheritance | Should -Be "ThisFolderOnly"
+      }
+      It "checks correct object being returned - two FMPermission" {
+         $Permission = (New-FMPermission -Id 'foo' -Perm 'Modify' -Inh 'ThisFolderOnly'),
+         (New-FMPermission -Id 'bar' -Perm 'Write' -Inh 'ThisFolderSubfoldersAndFiles')
+         $result = New-FMPathPermission -Path C:\Temp -InputObject $Permission
+         $result.Path | Should -Be "C:\Temp"
+         $result.Permission.Count | Should -be 2
+         $result.Permission.Identity | Should -Be ('foo', 'bar')
+         $result.Permission.Permission | Should -Be ('Modify', 'Write')
+         $result.Permission.Inheritance | Should -Be ("ThisFolderOnly", "ThisFolderSubfoldersAndFiles")
+      }
+   }#end context
+   Context 'Class FMPathPermission - helper function - paramset [Default]' -Tag Unit {
+      It "verify that helper funtion parameterset 'default' exists" {
+         {New-FMPathPermission -Path C:\Temp -Identity 'foo' -Permission 'Write' -Inheritance 'ThisFolderOnly'}  | Should -Not -Throw
+      }
+      It 'checks for correct object being returned' {
+         (New-FMPathPermission -Path C:\Temp -Identity 'foo' -Permission 'Write' -Inheritance 'ThisFolderOnly').Gettype() | Should -Be 'FMPathPermission'
+      }
+      It 'checks for correct object being returned - one path' {
+         $Result=New-FMPathPermission -Path C:\Temp -Identity 'foo' -Permission 'Write' -Inheritance 'ThisFolderOnly'
+         $result.Path | Should -Be 'C:\Temp'
+         $result.Permission.Count | Should -be 1
+         $result.Permission.Identity | Should -Be "foo"
+         $result.Permission.Permission | Should -Be "Write"
+         $result.Permission.Inheritance | Should -Be "ThisFolderOnly"
+      }
+      It 'checks for correct object being returned - two path' {
+         $Result=New-FMPathPermission -Path C:\Temp,D:\Temp -Identity foo -Permission Write -Inheritance ThisFolderOnly
+         $result.Path | Should -Be ('C:\Temp','D:\Temp')
+         $result.Permission.Count | Should -be 1
+         $result.Permission.Identity | Should -Be "foo"
+         $result.Permission.Permission | Should -Be "Write"
+         $result.Permission.Inheritance | Should -Be "ThisFolderOnly"
+      }
+      It "Should fail if parameter counts don't match" {
+{New-FMPathPermission -Path C:\Temp,D:\Temp -Identity foo -Permission Write,Read -Inheritance ThisFolderOnly} | Should -Throw "Counts of identities*"
+      }
+      It "checks for correct object being returned - two permissions" {
+         $Result=New-FMPathPermission -Path C:\Temp -Identity foo,bar -Permission Write,Read -Inheritance ThisFolderFiles,ThisFolderOnly
+         $Result.Permission.Count | Should -be 2
+         $result.Permission.Identity | Should -Be ('foo', 'bar')
+         $result.Permission.Permission | Should -Be ('Write', 'Read')
+         $result.Permission.Inheritance | Should -Be ("ThisFolderFiles", "ThisFolderOnly")
+      }
+      It "checks for correct object being returned - two permissions, two paths" {
+         $Result=New-FMPathPermission -Path C:\Temp,D:\Temp -Identity foo,bar -Permission Write,Read -Inheritance ThisFolderFiles,ThisFolderOnly
+         $Result.Permission.Count | Should -be 2
+         $result.Permission.Identity | Should -Be ('foo', 'bar')
+         $result.Permission.Permission | Should -Be ('Write', 'Read')
+         $result.Permission.Inheritance | Should -Be ("ThisFolderFiles", "ThisFolderOnly")
       }
    }#end context
 }
